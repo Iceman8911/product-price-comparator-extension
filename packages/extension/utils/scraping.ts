8c1a784a774d75e9e14e8a21e11af9574df044f5
Iceman8911 <wuchiJSS3@gmail.com>
2025-12-31 17:30:26 +0100

    feat: add rating string cleaner

--- packages/extension/utils/scraping.ts
+++ packages/extension/utils/scraping.ts
@@ -1,6 +1,23 @@
+import * as v from "valibot";
+import { ProductDataRatingSchema } from "@/models/product";
+import { PRODUCT_RATING_RANGE } from "@/shared/constants";
+
 const PERIOD_OR_COMMA_REGEX = /,|\./;
 
 /** For cleaning stuff like "1,234,566" */
 export function cleanPriceString(priceStr: string): number {
 	return Number(priceStr.replaceAll(PERIOD_OR_COMMA_REGEX, ""));
+}
+
+const RATING_SEPERATOR = "/";
+/** For cleaning stuff like "4.4/5", as well as "3.5" */
+export function cleanRatingString(ratingStr: string): ProductDataRatingSchema {
+	const [
+		numerator = PRODUCT_RATING_RANGE.MIN,
+		denominator = PRODUCT_RATING_RANGE.MAX,
+	] = ratingStr.split(RATING_SEPERATOR).map(Number);
+
+	const actualRating = (numerator / denominator) * PRODUCT_RATING_RANGE.MAX;
+
+	return v.parse(ProductDataRatingSchema, actualRating);
 }
