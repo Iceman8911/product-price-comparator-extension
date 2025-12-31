2095ed154209cd28ac105344f8ff6408a1469465
Iceman8911 <wuchiJSS3@gmail.com>
2025-12-31 17:12:50 +0100

    doc: comment

--- packages/extension/utils/scraping.ts
+++ packages/extension/utils/scraping.ts
@@ -1,5 +1,6 @@
 const PERIOD_OR_COMMA_REGEX = /,|\./;
 
+/** For cleaning stuff like "1,234,566" */
 export function cleanPriceString(priceStr: string): number {
 	return Number(priceStr.replaceAll(PERIOD_OR_COMMA_REGEX, ""));
 }
