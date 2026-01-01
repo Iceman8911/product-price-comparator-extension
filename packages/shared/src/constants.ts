1b0e4c6a3f41a98a0eeda5eed9fbd9bffe2c24f2
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-01 18:46:53 +0100

    refactor: remove flawed code
    this isn't practical, sniffing window globals is more efficient me
    thinks.

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,5 +1,3 @@
-import type { ProductDataSchema } from "./models/product";
-
 /** The collection of sites I personally optimised for, those not included will fall back to a generic catch-all approach */
 export enum SupportedSiteDomains {
 	KONGA = "www.konga.com",
@@ -7,21 +5,6 @@ export enum SupportedSiteDomains {
 	TEMU = "www.temu.com",
 	JIJI = "www.jumia.com.ng",
 }
-/** Calling `textContent` on the result of these queries should result in the string version of the value we're looking for */
-export const PRODUCT_SITE_SCRAPING_QUERY_SELECTOR = {
-	[SupportedSiteDomains.KONGA]: {
-		currency: "[class*=priceBoxPrice] span",
-		name: "[class*=productName]",
-		/** "1,323,453" */
-		price: "[class*=priceBoxPrice] div",
-		/** "4.4/5" */
-		rating: "[class*=customerReview_] p",
-	},
-} as const satisfies {
-	[key in SupportedSiteDomains]?: {
-		[key in keyof Omit<ProductDataSchema, "store">]: string;
-	};
-};
 
 export enum PRODUCT_RATING_RANGE {
 	MIN = 1,
