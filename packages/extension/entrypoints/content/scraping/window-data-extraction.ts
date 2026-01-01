ef6570d8fea57086801b900e6336e79ba34caf15
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-01 20:14:56 +0100

    feat: add jumia product data extractor

--- packages/extension/entrypoints/content/scraping/window-data-extraction.ts
+++ packages/extension/entrypoints/content/scraping/window-data-extraction.ts
@@ -1,7 +1,15 @@
 // Some shopping sites have the currently viewed product's data somewhere within a global
+/** biome-ignore-all lint/complexity/useLiteralKeys: <TS prefers "computed" key indexes> */
 
-import type { SupportedSitePatterns } from "@bandwidth-saver/shared";
-import type { ProductDataSchema } from "../../../../shared/src/models/product";
+import {
+	SCRAPED_PRODUCT_DATA_CLEANER,
+	SupportedSitePatterns,
+} from "@bandwidth-saver/shared";
+import * as v from "valibot";
+import {
+	ProductDataRatingSchema,
+	ProductDataSchema,
+} from "../../../../shared/src/models/product";
 
 /** Either scrapes or sniffs js globals or smth */
 type ProductDataExtractor = (
@@ -9,8 +17,51 @@ type ProductDataExtractor = (
 	siteWindow: Window & Record<string, any>,
 ) => ProductDataSchema | null;
 
-const SUPPORTED_SITE_PRODUCT_DATA_EXTRACTOR = {} as const satisfies {
-	[key in SupportedSitePatterns]: ProductDataExtractor;
+const SPACE_SEPERATOR = " ";
+
+const JumiaProductSchema = v.looseObject({
+	/** This includes the brand and name, e.g "XIAOMI REDMI A5 -  6.88   4GB RAM/128GB ROM  -- BLACK" instead of "REDMI A5 -  6.88   4GB RAM/128GB ROM  -- BLACK" */
+	displayName: v.string(),
+	prices: v.looseObject({
+		/** "₦ 104,871" */
+		price: v.string(),
+	}),
+	rating: v.looseObject({
+		average: ProductDataRatingSchema,
+	}),
+});
+
+const jumiaExtractor: ProductDataExtractor = (window) => {
+	try {
+		const { displayName, prices, rating } = v.parse(
+			JumiaProductSchema,
+			window["__STORE__"].products[0],
+		);
+
+		const [currency, dirtyPrice] = prices.price.split(SPACE_SEPERATOR);
+
+		if (!currency || !dirtyPrice) return null;
+
+		const productData = {
+			currency,
+			name: displayName,
+			price: SCRAPED_PRODUCT_DATA_CLEANER.price(dirtyPrice),
+			rating: rating.average,
+			store: "Jumia",
+		} as const satisfies ProductDataSchema;
+
+		return v.parse(ProductDataSchema, productData);
+	} catch (e) {
+		console.warn("Jumia extraction failed with error:", e);
+
+		return null;
+	}
+};
+
+const SUPPORTED_SITE_PRODUCT_DATA_EXTRACTOR = {
+	[SupportedSitePatterns.JUMIA]: jumiaExtractor,
+} as const satisfies {
+	[key in SupportedSitePatterns]?: ProductDataExtractor;
 };
 
 export default SUPPORTED_SITE_PRODUCT_DATA_EXTRACTOR;
