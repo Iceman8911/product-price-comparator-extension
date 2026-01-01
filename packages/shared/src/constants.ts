d7a2d0f8a0f33e54caa483f0b62374bddf6a72af
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-01 18:58:42 +0100

    feat: add more explicitly supported sites

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,9 +1,28 @@
-/** The collection of sites I personally optimised for, those not included will fall back to a generic catch-all approach */
-export enum SupportedSiteDomains {
-	KONGA = "www.konga.com",
-	JUMIA = "www.jumia.com",
-	TEMU = "www.temu.com",
-	JIJI = "www.jumia.com.ng",
+/** The collection of sites I personally optimised for, those not included will fall back to a generic catch-all approach.
+ */
+export enum SupportedSitePatterns {
+	JUMIA = "https://*.jumia.*/*",
+	KONGA = "https://*.konga.com/*",
+	JIJI = "https://jiji.ng/*",
+	TAKEALOT = "https://*.takealot.com/*",
+	KILIMALL = "https://*.kilimall.co.ke/*",
+	ZANDO = "https://*.zando.co.za/*",
+	SUPERBALIST = "https://*.superbalist.com/*",
+	BOB_SHOP = "https://*.bobshop.co.za/*",
+	PAYPORTE = "https://*.payporte.com/*",
+	KARA = "https://*.kara.com.ng/*",
+	SLOT = "https://*.slot.ng/*",
+
+	AMAZON_GLOBAL = "https://*.amazon.com/*",
+	AMAZON_ZA = "https://*.amazon.co.za/*",
+	TEMU = "https://*.temu.com/*",
+	ALIEXPRESS = "https://*.aliexpress.com/*",
+	SHEIN = "https://*.shein.com/*",
+	EBAY = "https://*.ebay.com/*",
+	WALMART = "https://*.walmart.com/*",
+	SHOPEE = "https://*.shopee.com/*",
+	ETSY = "https://*.etsy.com/*",
+	ALIBABA = "https://*.alibaba.com/*",
 }
 
 export enum PRODUCT_RATING_RANGE {
