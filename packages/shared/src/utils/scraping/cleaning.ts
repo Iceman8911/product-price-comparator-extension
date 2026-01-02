d3d50d2ac414e35c86348cb96b3dec3a8a086435
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-02 18:37:04 +0100

    feat: add image source cleaner

--- packages/shared/src/utils/scraping/cleaning.ts
+++ packages/shared/src/utils/scraping/cleaning.ts
@@ -1,9 +1,11 @@
 import * as v from "valibot";
+import PlaceholderImg from "../../assets/placeholder.webp";
 import { PRODUCT_RATING_RANGE } from "../../constants";
 import {
 	ProductDataRatingSchema,
 	type ProductDataSchema,
 } from "../../models/product";
+import { UrlSchema } from "../../models/shared";
 
 const PERIOD_OR_COMMA_REGEX = /,|\./;
 
@@ -29,9 +31,24 @@ function genericCleaner(str: string): string {
 	return str.trim();
 }
 
+function cleanImgSrcString(imgSrc: string): UrlSchema {
+	try {
+		return v.parse(UrlSchema, genericCleaner(imgSrc));
+	} catch {
+		console.warn(
+			"Image source string,",
+			imgSrc,
+			"could not be parsed properly",
+		);
+
+		return PlaceholderImg;
+	}
+}
+
 /** For cleaning and extracting proper data from scraped strings */
 export const SCRAPED_PRODUCT_DATA_CLEANER = {
 	currency: genericCleaner,
+	imgSrc: cleanImgSrcString,
 	name: genericCleaner,
 	price: cleanPriceString,
 	rating: cleanRatingString,
