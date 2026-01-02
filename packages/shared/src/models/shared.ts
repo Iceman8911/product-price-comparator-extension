44725e7b36a5fad0d35d5dcf0139de5692cdbaed
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-02 16:42:36 +0100

    refactor: de-brand `UrlSchema`

--- packages/shared/src/models/shared.ts
+++ packages/shared/src/models/shared.ts
@@ -9,7 +9,7 @@ export type NumberBetween1and100Inclusively = v.InferOutput<
 	typeof NumberBetween1and100Inclusively
 >;
 
-export const UrlSchema = v.pipe(v.string(), v.url(), v.brand("url"));
+export const UrlSchema = v.pipe(v.string(), v.url());
 export type UrlSchema = v.InferOutput<typeof UrlSchema>;
 
 export const DataUrlSchema = v.pipe(
