c5ecd5978371d9db28f91bd51b1ae6bdd64e0037
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-02 20:11:48 +0100

    feat: add utility combined product data extractor

--- packages/extension/entrypoints/content/scraping/extractors/shared.ts
+++ packages/extension/entrypoints/content/scraping/extractors/shared.ts
@@ -1,3 +1,4 @@
+import { fixCaughtErrorType } from "@bandwidth-saver/shared";
 import type { ProductDataSchema } from "../../../../../shared/src/models/product";
 
 /** Either scrapes or sniffs js globals or smth */
@@ -5,3 +6,36 @@ export type ProductDataExtractor = (
 	// biome-ignore lint/suspicious/noExplicitAny: <To cover sites that add extra props to the window object>
 	siteWindow: Window & Record<string, any>,
 ) => ProductDataSchema | null;
+
+export function createCombinedProductDataExtractor(
+	storeName: string,
+	windowGlobalExtractor: ProductDataExtractor,
+	documentScraperExtractor: ProductDataExtractor,
+): ProductDataExtractor {
+	const combinedExtractor: ProductDataExtractor = (window) => {
+		try {
+			return windowGlobalExtractor(window) ?? documentScraperExtractor(window);
+		} catch (e) {
+			console.warn(
+				storeName,
+				"window global or document scraper data extraction failed with error:",
+				fixCaughtErrorType(e),
+			);
+
+			try {
+				return documentScraperExtractor(window);
+			} catch (e) {
+				console.warn(
+					storeName,
+					" document scraper data extraction failed with error:",
+					fixCaughtErrorType(e),
+					".Falling back to `null",
+				);
+
+				return null;
+			}
+		}
+	};
+
+	return combinedExtractor;
+}
