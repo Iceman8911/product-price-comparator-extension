07920b7e7d6dd8457aba1b013a15effd716c1024
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-03 10:41:38 +0100

    feat: create shared document scraper creator helper

--- packages/extension/entrypoints/content/scraping/extractors/shared.ts
+++ packages/extension/entrypoints/content/scraping/extractors/shared.ts
@@ -1,11 +1,69 @@
-import { fixCaughtErrorType } from "@bandwidth-saver/shared";
-import type { ProductDataSchema } from "../../../../../shared/src/models/product";
+import {
+	fixCaughtErrorType,
+	SCRAPED_PRODUCT_DATA_CLEANER,
+} from "@bandwidth-saver/shared";
+import * as v from "valibot";
+import { ProductDataSchema } from "../../../../../shared/src/models/product";
 
 /** Either scrapes or sniffs js globals or smth */
 export type ProductDataExtractor = (
 	// biome-ignore lint/suspicious/noExplicitAny: <To cover sites that add extra props to the window object>
 	siteWindow: Window & Record<string, any>,
 ) => ProductDataSchema | null;
+
+type GetStringFromDocumentScraping = (document: Document) => string | undefined;
+type ProductDataDocumentScraperCallbacks = {
+	[key in `get${Capitalize<
+		keyof Omit<ProductDataSchema, "store">
+	>}`]: GetStringFromDocumentScraping;
+};
+
+export function createDocumentScraperProductDataExtractor(
+	arg: { storeName: string } & ProductDataDocumentScraperCallbacks,
+): ProductDataExtractor {
+	const { getCurrency, getImgSrc, getName, getPrice, getRating, storeName } =
+		arg;
+
+	const documentScraperExtractor: ProductDataExtractor = ({ document }) => {
+		const scrapedName = getName(document);
+		const scrapedCurrency = getCurrency(document);
+		const scrapedPrice = getPrice(document);
+		const scrapedRating = getRating(document);
+		const scrapedImgSrc = getImgSrc(document);
+
+		if (
+			!scrapedCurrency ||
+			!scrapedName ||
+			!scrapedPrice ||
+			!scrapedRating ||
+			!scrapedImgSrc
+		) {
+			console.warn(
+				"Undefined data in one of the variables:",
+				scrapedCurrency,
+				scrapedName,
+				scrapedPrice,
+				scrapedRating,
+				scrapedImgSrc,
+			);
+
+			return null;
+		}
+
+		const productData = {
+			currency: SCRAPED_PRODUCT_DATA_CLEANER.currency(scrapedCurrency),
+			imgSrc: SCRAPED_PRODUCT_DATA_CLEANER.imgSrc(scrapedImgSrc),
+			name: SCRAPED_PRODUCT_DATA_CLEANER.name(scrapedName),
+			price: SCRAPED_PRODUCT_DATA_CLEANER.price(scrapedPrice),
+			rating: SCRAPED_PRODUCT_DATA_CLEANER.rating(scrapedRating),
+			store: storeName,
+		} as const satisfies ProductDataSchema;
+
+		return v.parse(ProductDataSchema, productData);
+	};
+
+	return documentScraperExtractor;
+}
 
 export function createCombinedProductDataExtractor(
 	storeName: string,
