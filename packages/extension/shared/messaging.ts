5ae2115b1c953952936767673154b827eebeae98
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-04 17:09:49 +0100

    feat: add window-based messsaging

--- packages/extension/shared/messaging.ts
+++ packages/extension/shared/messaging.ts
@@ -1,6 +1,7 @@
 import { defineExtensionMessaging } from "@webext-core/messaging";
+import { defineWindowMessaging } from "@webext-core/messaging/page";
 import type { sendQueryToPhindAiViaBackgroundWorker } from "@/utils/phind";
-import { MessageType } from "./constants";
+import { EXTENSION_ID, MessageType } from "./constants";
 
 interface ProtocolMap {
 	[MessageType.SEND_PROMPT_TO_PHIND_AI_VIA_BACKGROUND_WORKER](
@@ -10,3 +11,8 @@ interface ProtocolMap {
 
 export const { onMessage, sendMessage } =
 	defineExtensionMessaging<ProtocolMap>();
+
+export const { onMessage: onWindowMessage, sendMessage: sendWindowMessage } =
+	defineWindowMessaging<ProtocolMap>({
+		namespace: EXTENSION_ID,
+	});
