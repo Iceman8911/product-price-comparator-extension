8911369121e91add14c3d909de273be2b301da53
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-04 15:11:14 +0100

    feat: add 4-pronged product data extraction approach

--- packages/shared/src/utils/scraping/extractors/index.ts
+++ packages/shared/src/utils/scraping/extractors/index.ts
@@ -1,8 +1,12 @@
 // Some shopping sites have the currently viewed product's data somewhere within a global
 
 import { SupportedSitePatterns } from "@bandwidth-saver/shared";
+import { MatchPattern } from "@webext-core/match-patterns";
+import type { ProductDataSchema } from "../../../models/product";
+import { llmProductDataExtractor } from "./fallback";
 import { jumiaProductDataExtractor } from "./jumia";
 import { kongaProductDataExtractor } from "./konga";
+import { schemaOrgProductDataExtractor } from "./schema.org";
 import type { ProductDataExtractor } from "./shared";
 import { temuProductDataExtractor } from "./temu";
 
@@ -14,4 +18,42 @@ const SUPPORTED_SITE_PRODUCT_DATA_EXTRACTOR = {
 	[key in SupportedSitePatterns]?: ProductDataExtractor;
 };
 
-export default SUPPORTED_SITE_PRODUCT_DATA_EXTRACTOR;
+const SUPPORTED_SITE_PRODUCT_DATA_MATCH_PATTERNS_AND_EXTRACTORS =
+	Object.entries(SUPPORTED_SITE_PRODUCT_DATA_EXTRACTOR).map(
+		(entry) => [new MatchPattern(entry[0]), entry[1]] as const,
+	);
+
+const backupProductDataExtractor = ([window, llmQuerier]: Parameters<
+	typeof llmProductDataExtractor
+>) => {
+	return (
+		schemaOrgProductDataExtractor(window) ??
+		llmProductDataExtractor(window, llmQuerier)
+	);
+};
+
+/** All encompassing product extractor */
+export const extractProductDataFromWindow = async ([
+	window,
+	llmQuerier,
+]: Parameters<
+	typeof llmProductDataExtractor
+>): Promise<ProductDataSchema | null> => {
+	const siteUrl = window.location.href;
+
+	const matchingSupportedExtractor =
+		SUPPORTED_SITE_PRODUCT_DATA_MATCH_PATTERNS_AND_EXTRACTORS.find(
+			([matchPattern]) => {
+				return matchPattern.includes(siteUrl);
+			},
+		);
+
+	if (matchingSupportedExtractor) {
+		return (
+			matchingSupportedExtractor[1](window) ??
+			backupProductDataExtractor([window, llmQuerier])
+		);
+	}
+
+	return backupProductDataExtractor([window, llmQuerier]);
+};
