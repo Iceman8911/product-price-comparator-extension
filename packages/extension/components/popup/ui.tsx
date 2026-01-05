82c66ac2566965be597d81dedbcd3930b546b465
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-05 15:53:18 +0100

    wip: add default ui for detecting products

--- packages/extension/components/popup/ui.tsx
+++ packages/extension/components/popup/ui.tsx
@@ -1,7 +1,32 @@
+import SearchIcon from "lucide-solid/icons/search";
+
+function DetectProductOnCurrentSiteButton() {
+	return (
+		<BaseButton
+			aria-label="Detect Product on Current Site"
+			class="btn-circle btn-primary size-14"
+		>
+			<SearchIcon class="size-8" />
+		</BaseButton>
+	);
+}
+
+function NoProductDetectedOnCurrentSiteYetUi() {
+	return (
+		<div class="flex flex-col items-center justify-center gap-8">
+			<h2 class="text-center text-base">
+				Seems like no product has been autodetected. Click the button below to
+				begin manual detection.
+			</h2>
+			<DetectProductOnCurrentSiteButton />
+		</div>
+	);
+}
+
 export default function PopupUi() {
 	return (
-		<div class="grid size-full place-items-center rounded-box bg-base-200 p-4">
-			Bob
+		<div class="glass grid size-full place-items-center rounded-box bg-base-200 p-4">
+			<NoProductDetectedOnCurrentSiteYetUi />
 		</div>
 	);
 }
