d4451b768bb9ff45178565ed4986687fde2c536b
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-05 14:33:15 +0100

    wip: reset popup

---
+++ packages/extension/components/popup/ui.tsx
@@ -0,0 +1,7 @@
+export default function PopupUi() {
+	return (
+		<div class="grid size-full place-items-center rounded-box bg-base-200 p-4">
+			Bob
+		</div>
+	);
+}
--- packages/extension/entrypoints/popup/App.tsx
+++ packages/extension/entrypoints/popup/App.tsx
@@ -1,77 +1,9 @@
-import { getRandomUUID, type UrlSchema } from "@bandwidth-saver/shared";
-import { createAsync } from "@solidjs/router";
-import { createSignal } from "solid-js";
-import { PopupCompressionSettings } from "@/components/popup/compression-settings";
-import { PopupFooter } from "@/components/popup/footer";
-import { PopupHeader } from "@/components/popup/header";
-import { PopupOtherSettings } from "@/components/popup/other-settings";
-import { PopupProxySettings } from "@/components/popup/proxy-settings";
-import { PopupStatistics } from "@/components/popup/statistics";
-import { PopupToggles } from "@/components/popup/toggle";
-import type { SettingsScope } from "@/models/context";
-import { DEFAULT_GENERAL_SETTINGS } from "@/models/storage";
-import { DUMMY_TAB_URL, getActiveTabUrl } from "@/shared/constants";
-import { getSiteSpecificGeneralSettingsStorageItem } from "@/shared/storage";
-
-function PopupSettings(props: { scope: SettingsScope; tabUrl: UrlSchema }) {
-	const accordionName = getRandomUUID();
-
-	return (
-		<div class="join join-vertical bg-base-100">
-			<PopupCompressionSettings
-				name={accordionName}
-				scope={props.scope}
-				tabUrl={props.tabUrl}
-			/>
-
-			<PopupProxySettings
-				name={accordionName}
-				scope={props.scope}
-				tabUrl={props.tabUrl}
-			/>
-
-			<PopupOtherSettings
-				name={accordionName}
-				scope={props.scope}
-				tabUrl={props.tabUrl}
-			/>
-
-			{/*<Show when={siteSpecificGeneralSettings().useDefaultRules}>
-				<div class="absolute z-10 size-full bg-black opacity-25"></div>
-			</Show>*/}
-		</div>
-	);
-}
+import PopupUi from "@/components/popup/ui";
 
 export default function App() {
-	const activeTabUrl = createAsync(getActiveTabUrl, {
-		initialValue: DUMMY_TAB_URL,
-	});
-
-	const [scope, setScope] = createSignal<SettingsScope>("site");
-
-	const siteSpecificGeneralSettings = convertStorageItemToReactiveSignal(
-		() => getSiteSpecificGeneralSettingsStorageItem(activeTabUrl()),
-		DEFAULT_GENERAL_SETTINGS,
-	);
-
-	const areSiteSpecificRulesEnabled = () =>
-		siteSpecificGeneralSettings().enabled &&
-		siteSpecificGeneralSettings().ruleIdOffset != null;
-
 	return (
-		<div class="h-fit w-96 divide-y divide-base-300 *:w-full *:p-4">
-			<PopupHeader scope={scope()} setScope={setScope} />
-
-			<PopupStatistics scope={scope()} tabUrl={activeTabUrl()} />
-
-			<PopupToggles scope={scope()} tabUrl={activeTabUrl()} />
-
-			<Show when={areSiteSpecificRulesEnabled() || scope() === "default"}>
-				<PopupSettings scope={scope()} tabUrl={activeTabUrl()} />
-			</Show>
-
-			<PopupFooter />
+		<div class="aspect-4/5 h-100 p-4">
+			<PopupUi />
 		</div>
 	);
 }
