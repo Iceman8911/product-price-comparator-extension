39adb06a04ad478b34f89af883d6e28d85e6b126
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-06 19:32:04 +0100

    fix: adjust popup aspect ratio

--- packages/extension/entrypoints/popup/App.tsx
+++ packages/extension/entrypoints/popup/App.tsx
@@ -2,7 +2,7 @@ import PopupUi from "@/components/popup/base-ui";
 
 export default function App() {
 	return (
-		<div class="aspect-5/4 h-100 p-4">
+		<div class="aspect-9/10 h-135 p-4">
 			<PopupUi />
 		</div>
 	);
