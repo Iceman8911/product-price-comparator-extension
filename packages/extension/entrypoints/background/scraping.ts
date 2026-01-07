e04329eb4b6af712f30b00a6197623131046b620
Iceman8911 <wuchiJSS3@gmail.com>
2026-01-07 19:08:30 +0100

    wip: remove DOMParser parsing
    the created Document obejct isn't what Defuddle expects anyway :p. lots
    of ugly bugs from this.

--- packages/extension/entrypoints/background/scraping.ts
+++ packages/extension/entrypoints/background/scraping.ts
@@ -1,8 +1,6 @@
 import {
 	clone,
-	extractProductDataFromDocumentOrWindow,
 	fixCaughtErrorType,
-	sendQueryToPhindAi,
 	type UrlSchema,
 } from "@shopping-optimizer/shared";
 import {
@@ -22,71 +20,6 @@ const DEFAULT_PARTIALLY_PROCESSED_RESULTS = {
 	pendingUrls: [],
 	products: [],
 } as const satisfies PartiallyProcessedProductResults;
-
-const domParser = new DOMParser();
-
-async function extractProductDataFromUrlsViaSimpleDomParsing(
-	enableAi: boolean,
-	...urls: ReadonlyArray<UrlSchema>
-): Promise<PartiallyProcessedProductResults> {
-	try {
-		const siteDocumentsAndUrls = await Promise.allSettled(
-			urls.map((url) =>
-				fetch(url)
-					.then((res) => res.text())
-					.then((html) => ({
-						doc: domParser.parseFromString(html, "text/html"),
-						url,
-					})),
-			),
-		).then((settledResults) =>
-			settledResults.reduce<{ doc: Document; url: UrlSchema }[]>(
-				(successfulResults, settledResult) => {
-					if (settledResult.status === "fulfilled")
-						successfulResults.push(settledResult.value);
-
-					return successfulResults;
-				},
-				[],
-			),
-		);
-
-		const partiallyProcessedProductResults: PartiallyProcessedProductResults =
-			clone(DEFAULT_PARTIALLY_PROCESSED_RESULTS);
-
-		const productDataExtractionPromises: Promise<void>[] = [];
-
-		for (const { doc, url } of siteDocumentsAndUrls) {
-			const promise = extractProductDataFromDocumentOrWindow([
-				doc,
-				enableAi
-					? (...queries) =>
-							sendQueryToPhindAi(
-								...queries.map((query) => ({ query, search: false })),
-							)
-					: undefined,
-				url,
-			]).then((possibleProduct) => {
-				if (possibleProduct)
-					partiallyProcessedProductResults.products.push(possibleProduct);
-				else partiallyProcessedProductResults.pendingUrls.push(url);
-			});
-
-			productDataExtractionPromises.push(promise);
-		}
-
-		await Promise.all(productDataExtractionPromises);
-
-		return partiallyProcessedProductResults;
-	} catch (e) {
-		console.error(
-			"Background script failed to extract product data from html with error:",
-			fixCaughtErrorType(e),
-		);
-
-		return clone(DEFAULT_PARTIALLY_PROCESSED_RESULTS);
-	}
-}
 
 export async function extractProductDataFromUrls(
 	...urls: ReadonlyArray<UrlSchema>
@@ -114,15 +47,7 @@ export async function extractProductDataFromUrls(
 			}
 		}
 
-		const extractedProductsViaHtmlParsing =
-			await extractProductDataFromUrlsViaSimpleDomParsing(
-				shouldEnableAi,
-				...partiallyProcessedProductResults.pendingUrls,
-			);
-
-		return partiallyProcessedProductResults.products.concat(
-			extractedProductsViaHtmlParsing.products,
-		);
+		return partiallyProcessedProductResults.products;
 	} catch (e) {
 		console.error(
 			"Background script failed to extract product data from url:",
